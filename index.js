56e6da7041f30f55d0c9f27fef94a8c5090ef14c
jessicasimplicio <jessicasimplicio1@gmail.com>
2018-09-06 14:30:42 -0300

    testes implementados

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function getLinksFromMd(str){
+module.exports.getLinksFromMd = function getLinksFromMd(str){
 
-
-  if(typeof str === undefined){
+  if(typeof str === "undefined"){
     throw new TypeError ("Não pode ser vazio. Insira um texto.");
   }
 
@@ -18,7 +17,7 @@ function getLinksFromMd(str){
       new RegExp(/(https?:\/\/)?(www\.)?(\w{0,1000}\.)?[a-z0-9]+\.\w{2,3}(\.br)?(\/\w{0,1000}\/\w{0,1000})?/g);
     let urls = str.match(regexUrl);
 
-    let regexTextUrl = new RegExp(/\[\w{0,1000}\]/g);
+    let regexTextUrl = new RegExp( /(?<=\[)\w{0,50}(?=\])/g ) 
     let textUrl = str.match(regexTextUrl);
 
     let urlAndText = [];
@@ -35,21 +34,4 @@ function getLinksFromMd(str){
     return urlAndText; 
   }
 }
-
-/*getLinksFromMd("# Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit," +
-"sed do eiusmod tempor  incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et " +
-"[dolore](https://en.wiktionary.org/wiki/dolore) magna aliqua. Ut enim ad minim veniam, " +
-	"quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
-
-"[foo](http://foo.com)" +
-
-"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat " +
-"nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
-"deserunt mollit anim id est laborum.");
-*/
-
-//getLinksFromMd( "Oi você quer entrar no site [google](www.google.com) ?");
-
-getLinksFromMd();
-module.export = getLinksFromMd;
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -10,7 +10,6 @@ describe("index", function() {
 
     describe("when it doesn't have parameter", function() {
       it("should throw an error", function() {
-        //expect(() => index.getLinksFromMd("")).to.throw(TypeError);
         expect(() => index.getLinksFromMd()).to.throw("Não pode ser vazio. Insira um texto.");
       });
     });
@@ -34,16 +33,33 @@ describe("index", function() {
 
       describe("and it has one URL", function() {
         it("should return an array with one object", function() {
-          //expect(() => index.cardValidator(2)).to.throw(TypeError);
+          expect(index.getLinksFromMd("Oi você quer entrar no site [google](www.google.com) ?"))
+            .to.deep.equal([{href: "www.google.com", text: "google"}]);
+
+          expect(index.getLinksFromMd("Oi você quer comer [momolamen](www.momolamen.com.br) ?"))
+            .to.deep.equal([{href: "www.momolamen.com.br", text: "momolamen"}]);
         });
       }); 
 
       describe("and it has more than one URLs", function() {
         it("should return an array with objects", function() {
-          
-                              
+          expect(index.getLinksFromMd("# Lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing" +
+            "elit, sed do eiusmod tempor  incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et " +
+            "[dolore](https://en.wiktionary.org/wiki/dolore) magna aliqua. Ut enim ad minim veniam, " +
+            "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+            "[foo](http://foo.com)" +
+            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat " +
+            "nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
+            "deserunt mollit anim id est laborum."))
+            .to.deep.equal([
+              { href: 'https://en.wiktionary.org/wiki/labore', text: 'labore' },
+              { href: 'https://en.wiktionary.org/wiki/dolore', text: 'dolore' },
+              { href: 'http://foo.com', text: 'foo' },
+            ]);
+
         });
       });
+
     });
 
   });
